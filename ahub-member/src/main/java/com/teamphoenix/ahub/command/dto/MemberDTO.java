5e258aded4d6bb426030d22f8a114a2645127eca
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-03-06 16:03:40 +0900

    Service 계층에 회원 가입, 회원 정보 수정 기능 구현
    Service 계층에 회원 가입, 회원 정보 수정 기능 구현

--- ahub-member/src/main/java/com/teamphoenix/ahub/command/dto/MemberDTO.java
+++ ahub-member/src/main/java/com/teamphoenix/ahub/command/dto/MemberDTO.java
@@ -1,4 +1,17 @@
 package com.teamphoenix.ahub.command.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class MemberDTO {
+    private int memberCode;
+    private String memberId;
+    private String memberName;
+    private String memberPwd;
+    private String memberEmail;
+    private String memberAddr;
+    private String memberPhone;
+    private LocalDateTime restrictStartDate;
 }
--- ahub-member/src/main/java/com/teamphoenix/ahub/command/service/MemberService.java
+++ ahub-member/src/main/java/com/teamphoenix/ahub/command/service/MemberService.java
@@ -1,4 +1,12 @@
 package com.teamphoenix.ahub.command.service;
 
-public class MemberService {
+import com.teamphoenix.ahub.command.dto.MemberDTO;
+
+public interface MemberService {
+
+    void registMember(MemberDTO newMemberInfo);
+
+    void modifyMember(String memberId, MemberDTO modifyMember);
+
+    void removeMember(String memberId);
 }
