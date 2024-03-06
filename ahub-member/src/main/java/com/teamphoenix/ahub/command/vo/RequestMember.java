7fca43d21e1173882d68b4f1e4b893679f26ed73
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-03-06 16:02:32 +0900

    기능 구현을 위한 Entity, DTO, VO 추가
    MemberInfo 엔티티, memberDTO, requestMember와 responseMember VO 생성

---
+++ ahub-member/src/main/java/com/teamphoenix/ahub/command/vo/RequestMember.java
@@ -0,0 +1,17 @@
+package com.teamphoenix.ahub.command.vo;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RequestMember {
+
+        private String memberId;
+        private String memberPwd;
+        private String memberName;
+        private String memberAddr;
+        private String memberPhone;
+}
