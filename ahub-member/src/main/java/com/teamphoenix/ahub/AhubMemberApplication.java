de8e132b4c3f039dc08946c3ce0d741254488e1b
t4e1 <jtw5738@naver.com>
2024-03-08 17:24:21 +0900

    2024.03.08 프로젝트 업데이트
    member 쪽 어노테이션 제거

--- ahub-member/src/main/java/com/teamphoenix/ahub/AhubMemberApplication.java
+++ ahub-member/src/main/java/com/teamphoenix/ahub/AhubMemberApplication.java
@@ -4,10 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 import org.springframework.context.annotation.Bean;
 
-@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AhubMemberApplication {
